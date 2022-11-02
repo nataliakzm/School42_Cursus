003123bb03d79ade4f918e674a730aafaa827e37
nataliakzm <natkuzminykh94@gmail.com>
2022-11-02 18:54:42 +0100

    Some updates

--- Ft_Printf/ft_printf.c
+++ Ft_Printf/ft_printf.c
@@ -5,12 +5,14 @@ int	ft_printf(const char *string, ...)
 	va_list	args;
 	char	*name;
 	unsigned int	days;
+	unsigned int	count;
 
-	va_start (args, string);
+	count = 0;
+	va_start(args, string);
 	name = va_arg(args, char *);
 	days = va_arg(args, int);
 	ft_printf(name);
 	ft_printf(days);
 	va_end(args);
-	return (0);		
+	return (0);	
 }
