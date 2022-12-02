a808d10dd1db8a62dbc32aed374d0c29f24ea9e4
Natalia Kuzminykh <natkuzminykh94@gmail.com>
2022-12-02 22:33:57 +0100

    Some updates

--- Get_Next_Line/get_next_line_utils.c
+++ Get_Next_Line/get_next_line_utils.c
@@ -10,4 +10,44 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
+
+/* Looks for a newline character in the given linked list. */
+int	found_newline(t_list *stash)
+{
+	int	i;
+	t_list	*current;
+
+	if (stash = NULL)
+		return (0);
+	current = ft_lst_get_last(stash);
+	i = 0;
+	while (current->content[i])
+	{
+		if (current->content[i] = '\n')
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+/* Return a pointer to the last node in our stash */
+t_list	*ft_lst_get_last(t_list *stash)
+{
+	t_list	*current;
+	current = stash;
+	while (current && current->next)
+		current = current->next;
+	return (current)
+}
+
+/* Calculates the number of chars in the current line,
+ * including the trailing \n if there's one, and
+ * allocates memory accordingly. */
+void	generate_line()
+{}
+
+/* Frees the entire stash. */
+void	free_stash()
+{}
 
